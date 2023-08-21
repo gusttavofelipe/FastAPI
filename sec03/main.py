4b565e8f14c4d81b29ec607c01c36a4a6faf8b90
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2023-08-21 11:25:53 -0300

    add- customize docs, upd- using model

--- sec03/main.py
+++ sec03/main.py
@@ -1,12 +1,17 @@
 from typing import Optional, Any
 from time import sleep
 from fastapi.responses import JSONResponse, Response
-from sec03.models import Course
+from sec03.models import Course, courses
 from fastapi import (
     FastAPI, HTTPException, Depends, Path, Query, Header, status
 )
 
-app = FastAPI()
+app = FastAPI(
+    # docs customization
+    title='API de cursos',
+    description='API para estudo',
+    version='0.0.1'
+)
 
 
 def fake_db():
@@ -18,33 +23,20 @@ def fake_db():
         sleep(1)
 
 
-courses = {
-    1: {
-        'title': 'Course 1',
-        'classes': 112,
-        'hours': 12
-    },
-    2: {
-        'title': 'Course 2',
-        'classes': 12,
-        'hours': 3
-    },
-    3: {
-        'title': 'Course 3',
-        'classes': 117,
-        'hours': 13
-    },
-}
-
-
-@app.get('/courses')
+@app.get(
+    '/courses', summary='Return all courses',
+    description='Return all courses or a empty dictionary',
+    response_model=list[Course])
 async def get_courses(db: Any = Depends(fake_db)):
-    return courses            
+    return courses
 # Depends adiciona uma dependencia a um recurso
 # (executa algo antes de chamar o recurso)
 
 
-@app.get('/courses/{course_id}')  # todo input vem como str
+@app.get(
+    '/courses/{course_id}',
+    description='Return a course from a course dictionary',
+    response_model=Course)
 async def get_course(
         db: Any = Depends(fake_db),
         course_id: int = Path(
@@ -61,7 +53,11 @@ async def get_course(
 # por padrão o status_code para post é 200
 
 
-@app.post('/courses', status_code=status.HTTP_201_CREATED)
+@app.post(
+    '/courses',
+    status_code=status.HTTP_201_CREATED,
+    description='add a course in the dictionary',
+    response_model=list[dict])
 async def post(course: Course, db: Any = Depends(fake_db)):
     course_id = len(courses) + 1
     courses[course_id] = course
@@ -69,7 +65,7 @@ async def post(course: Course, db: Any = Depends(fake_db)):
     return course
 
 
-@app.put('/courses/{course_id}')
+@app.put('/courses/{course_id}', description='update a course')
 async def put(course: Course, course_id: int, db: Any = Depends(fake_db)):
     if course_id in courses:
         courses[course_id] = course
@@ -83,7 +79,7 @@ async def put(course: Course, course_id: int, db: Any = Depends(fake_db)):
         )
 
 
-@app.delete('/courses/{course_id}')
+@app.delete('/courses/{course_id}', description='delete a course')
 async def delete(course_id: int, db: Any = Depends(fake_db)):
     if course_id in courses:
         del courses[course_id]
--- sec03/models.py
+++ sec03/models.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 from typing import Optional
 from pydantic import BaseModel
 
@@ -6,3 +7,11 @@ class Course(BaseModel):
     title: str
     classes: int
     hours: int
+
+
+courses = [
+    Course(id=1, title='Class 1', classes=45, hours=30),
+    Course(id=2, title='Class 2', classes=43, hours=50),
+    Course(id=3, title='Class 3', classes=42, hours=23),
+]
+pprint(courses.index(1))
