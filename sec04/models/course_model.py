05c660ec82e1f66d40392c203018896429f587aa
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2023-08-22 15:26:21 -0300

    criacao do model Coursemodel

---
+++ sec04/models/course_model.py
@@ -0,0 +1,11 @@
+from core.configs import settings
+from sqlalchemy import Column, Integer, String
+
+
+class CourseModel(settings.DBBasemodel): # database table
+    __table_name = 'courses'
+
+    id: str = Column(Integer, primary_key=True, autoincrement=True)
+    title: str = Column(String(255))
+    number_classes: str = Column(Integer)
+    hours: int = Column(Integer)
