fbec04e46cf97ed2be3e4a208d952a31604d6533
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2023-08-22 11:17:24 -0300

    trabalhando no core

---
+++ sec04/core/configs.py
@@ -0,0 +1,15 @@
+from pydantic import BaseSettings, Field
+from sqlalchemy.ext.declarative import declarative_base
+
+
+class Settings(BaseSettings):
+    '''General settings used in the application'''
+
+    API_VI_STR: str = '/api/v1'
+    DB_URL: str = 'postgresql+asyncpg://gustavo:gustavo@localhost:5432/fastapi1'
+    DBBasemodel = declarative_base()
+
+    class Config:
+        case_sensitive = True
+
+settings = Settings()
