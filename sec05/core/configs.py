33f0697c37fb3570b4b5184708b602e03d42dd98
Gustavo Felipe <gustavofeliperepositorio@gmail.com>
2023-09-04 09:59:03 -0300

    modulo core

---
+++ sec05/core/configs.py
@@ -0,0 +1,14 @@
+# arquivo de configuracoes do db
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    API_V1_STR: str = 'api/v1'
+    DB_URL: str = 'postgresql+asyncpg://gustavo:gustavo@localhost:5432/fastapi1'
+
+    class Config:
+        case_sensitive = True
+
+
+settings: Settings = Settings()
